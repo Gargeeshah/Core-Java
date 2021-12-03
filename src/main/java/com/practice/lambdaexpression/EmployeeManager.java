8626c02e7e74ab589899887052dc2da13d2080a2
Gargeeshah <gargeeshah@gmail.com>
2021-12-03 16:01:40 +0530

    Predefine functional interface example

--- src/main/java/com/practice/lambdaexpression/EmployeeManager.java
+++ src/main/java/com/practice/lambdaexpression/EmployeeManager.java
@@ -16,15 +16,5 @@ public class EmployeeManager {
     public List<Employee> getEmployees(){
         return employees;
     }
-    public void checkSalary(List<Employee> employees){
-        Predicate<Employee> employeePredicate = e1 -> e1.getSalary() > 40000.0;
-        Predicate<Employee> employeePredicate1 = e1 -> e1.getSalary() < 50000.0;
-        Predicate<Employee> filter = employeePredicate.and(employeePredicate1);
-        for(Employee e: employees){
-            if(filter.test(e))
-            {
-                System.out.println(e.getName()+"'s salary is greater than 40k and less than 50k");
-            }
-        }
-    }
+   
 }
