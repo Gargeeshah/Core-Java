701a958ad665157fbddd277eea18e95da7c65966
Gargeeshah <gargeeshah@gmail.com>
2021-12-07 11:47:21 +0530

    Executor framework example

---
+++ src/main/java/com/practice/executorframework/ExecuterServiceDemo.java
@@ -0,0 +1,42 @@
+package com.practice.executorframework;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class PrintJob implements Runnable
+{
+    String name;
+    PrintJob(String name){
+        this.name = name;
+
+    }
+    @Override
+    public void run() {
+        System.out.println(name+" Job started by thread "+Thread.currentThread().getName());
+        try {
+            Thread.sleep(5000);
+        }
+        catch(InterruptedException e) {}
+        System.out.println(name+" job completed by thread "+Thread.currentThread().getName());
+    }
+
+}
+public class ExecuterServiceDemo {
+
+    public static void main(String[] args) {
+
+        PrintJob[] jobs = {new PrintJob("abc"),
+                new PrintJob("pqr"),
+                new PrintJob("abcde"),
+                new PrintJob("xyz")
+        };
+
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        for(PrintJob job:jobs) {
+            service.submit(job);
+        }
+        service.shutdown();
+    }
+
+}
+
